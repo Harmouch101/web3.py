f59f311d38f11d803c176e31406f85baeaec9a92
Harmouch101 <eng.mahmoudharmouch@gmail.com>
2022-02-16 12:23:16 +0200

    :hammer: Make the `loop` argument of the `wait_for_ws` method optional and display a nicely formatted deprecation warning message.
    Signed-off-by: Harmouch101 <eng.mahmoudharmouch@gmail.com>

--- tests/utils.py
+++ tests/utils.py
@@ -3,19 +3,29 @@ import socket
 import time
 
 import websockets
+import warnings
 
 
 def get_open_port():
     sock = socket.socket()
-    sock.bind(('127.0.0.1', 0))
+    sock.bind(("127.0.0.1", 0))
     port = sock.getsockname()[1]
     sock.close()
     return str(port)
 
 
-async def wait_for_ws(endpoint_uri, timeout=10):
+async def wait_for_ws(endpoint_uri, timeout=10, event_loop=None):
+    if event_loop is not None:
+        warnings.warn(
+            "The event_loop parameter is deprecated and was removed "
+            "from websocket provider as of web3 v6. Consider calling "
+            "this method without passing this argument instead.",
+            category=DeprecationWarning,
+            stacklevel=2,
+        )
     start = time.time()
-    while time.time() < start + timeout:
+    stop = start + timeout
+    while time.time() < stop:
         try:
             async with websockets.connect(uri=endpoint_uri):
                 pass
